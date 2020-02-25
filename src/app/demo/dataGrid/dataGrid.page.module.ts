52944257d82b2395ce6513490c15883ab2739349
Mohamad Reza Pourghorban <mr.pourghorban@POURGHORBAN1>
2020-02-25 14:23:03 +0330

    grid edit

--- src/app/demo/dataGrid/dataGrid.page.module.ts
+++ src/app/demo/dataGrid/dataGrid.page.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataGridPage } from './dataGrid.page';
 import { AXDataGridModule } from '@acorex/data-grid';
-import { AXDataSourceModule } from '@acorex/components'
-
+import { AXDataSourceModule, AXPageModule, AXToolbarModule, AXFilterModule } from '@acorex/components';
+import { AXDockLayoutModule } from '@acorex/layout';
 @NgModule({
     declarations: [DataGridPage],
-    imports: [CommonModule, AXDataGridModule, AXDataSourceModule],
+    imports: [CommonModule, AXDataGridModule, AXDataSourceModule, AXPageModule, AXDockLayoutModule, AXToolbarModule, AXFilterModule],
     exports: [DataGridPage],
     providers: [],
 })
